e5b839b497c47e357d5b11559ce3a02d6deec95e
Kerry <cobbkerry@gmail.com>
2020-11-28 16:07:40 -0600

    Added checks for input, Added code to reverse barcodes

--- generate_barcodes.py
+++ generate_barcodes.py
@@ -19,8 +19,16 @@ def generate_files(input_csv, read1_enzyme, read2_enzyme, reverse_read1=False,
 
     Generates 1 output file containing all data.
     Generates a barcodes file for each plate.
-   
+
+    Use the reverse options if you loaded the adapters on to a plate backwards
     """ 
+    # Check inputs
+    if read1_enzyme not in ["nhei", "clai"]:
+        quit("Error: {} not a valid option for read1_enzyme. Choose one of: nhei, clai")
+
+    if read2_enzyme not in ["ecori", "bamhi", "hindiii"]:
+        quit("Error: {} not a valid option for read1_enzyme. Choose one of: ecori, bamhi, hindiii")
+
     df = pd.read_csv(input_csv)
 
     # Old barcode sequence reference not used now 
@@ -32,26 +40,32 @@ def generate_files(input_csv, read1_enzyme, read2_enzyme, reverse_read1=False,
 
     # Separate barcodes into read1 and read2 barcode data frames
     read1_df = barcodes_df[barcodes_df["read"] == "read1"].drop(
-        columns=["read"])
+            columns=["read"])
     read2_df = barcodes_df[barcodes_df["read"] == "read2"].drop(
-        columns=["read"])
+            columns=["read"])
 
     # Select subset of barcodes for input enzymes
     read1_df = read1_df[read1_df["enzyme"] == read1_enzyme]
     read2_df = read2_df[read2_df["enzyme"] == read2_enzyme]
 
+    # Reverse barcode order
+    if reverse_read1:
+        read1_df["position"] = read1_df["position"].values[::-1]
+    if reverse_read2:
+        read2_df["position"] = read2_df["position"].values[::-1]
+    
     # Change type of position column for read2_barcode_df
     read2_df["position"] = read2_df["position"].apply(pd.to_numeric)
 
     # Rename "barcode_sequence" columns
     read1_df = read1_df.rename(columns={
-      "barcode_sequence": "read1_barcode_sequence",
-      "barcode_id": "read1_barcode_id",
-      "position": "row"})
+        "barcode_sequence": "read1_barcode_sequence",
+        "barcode_id": "read1_barcode_id",
+        "position": "row"})
     read2_df = read2_df.rename(columns={
-      "barcode_sequence": "read2_barcode_sequence",
-      "barcode_id": "read2_barcode_id",
-      "position": "column"})
+        "barcode_sequence": "read2_barcode_sequence",
+        "barcode_id": "read2_barcode_id",
+        "position": "column"})
 
     # Merge selected barcodes with input data frame 
     df = df.merge(read1_df, on="row")
@@ -72,4 +86,3 @@ def generate_files(input_csv, read1_enzyme, read2_enzyme, reverse_read1=False,
 
 if __name__ == "__main__":
     fire.Fire(generate_files)
-
